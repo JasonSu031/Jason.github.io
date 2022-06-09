0b1596a72ec4eb070b4dc1c46840aa95d7f5f1cf
Jason <jasonsu9323@gmail.com>
2022-06-09 01:22:32 -0400

    finished js

--- Bootstrap-Vet/load.js
+++ Bootstrap-Vet/load.js
@@ -12,3 +12,29 @@ function toggleSpinner(name) {
     showSpinner(name);
     setTimeout(hideSpinner, 1500, name);
 }
+
+var curToast = 0;
+function book(){
+    if(curToast==4)maxToasts();
+    curToast++;
+    setTimeout(displayToast,1510)
+}
+function displayToast(){
+    var d = document.getElementById(curToast.toString());
+    d.style.display = "block";
+}
+function reorganize(){
+    curToast--;
+    for(let i =1; i<=curToast; i++){
+        var d = document.getElementById(i.toString());
+        d.style.display = "block";
+    }
+    for(let j = curToast+1; j<5; j++){
+        var d = document.getElementById(j.toString());
+        d.style.display = "none";
+    }
+}
+
+function maxToasts(){
+    alert("You can only have at most 4 Appointment notifications on your screen.\n Please clear some notifications before you book another appointment.");
+}
